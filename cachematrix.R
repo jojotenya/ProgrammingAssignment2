a478e9658e59f3e16bd0865d2bf0f3e27764e73a
jojotenya <jojotenya@gmail.com>
2014-10-19 07:22:48 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,31 +4,40 @@
 ## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+    M <- NULL
+    get <- function() x
+    inverse <- function(){
+        n <- nrow(x)
+        m <- ncol(x)
+        M <- matrix(,m,n)
+        for(i in 1:n){
+            for(j in 1:m){
+                M[j, i] <<- x[i,j]
+            }
 
-inverse <- function(){
-y <- matrix(,ncol(x),nrow(x))
-}
-
-
-
-}
-
-
-## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
-## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should 
-## retrieve the inverse from the cache.
-
-
-cacheSolve <- function(x, ...) {
+        }
+        M
+    }
+    get_inverse <- function() M
+        
+        list(inverse = inverse, get = get, get_inverse = get_inverse)
+        
+        
+    }
+    
+    
+    ## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+    ## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should
+    ## retrieve the inverse from the cache.
+    
+    
+    cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        if(!is.null()) {
-        print"target already been inversed"
-        return() 
+        M <- x$get_inverse()
+        if(!is.null(M)) {
+            message("target's already been inversed")
+            return(M)
         }
         x$inverse()
-        y[1,1]<-x[1,1]
-        y[1,2]<-x[2,1]
-        
-        
-        
-}
+        x$get_inverse()
+    }
